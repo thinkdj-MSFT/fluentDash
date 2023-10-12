4ecf502af7aa1ca3c860485fbe56d8f3c119614d
thinkdj <think.dj@gmail.com>
2023-10-12 11:24:26 +0530

    FluentProvider client side

---
+++ src/app/FluentProvider.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from 'react';
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
+
+export default function FluentProviderClient({children}: {children: React.ReactNode}) {
+	return (
+		<FluentProvider theme={webLightTheme}>
+			{children}
+		</FluentProvider>
+	);
+}
