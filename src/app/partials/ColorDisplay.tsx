5b077f4c680fad64edb759c2b62bbb8a7bc840af
thinkdj <think.dj@gmail.com>
2023-10-18 15:46:20 +0530

    ColorDisplay Block: hexToRGBA feat enh.

--- src/app/partials/ColorDisplay.tsx
+++ src/app/partials/ColorDisplay.tsx
@@ -5,6 +5,21 @@ export default function ColorDisplay (props:any) {
 	const isColObj = props?.color?.theme ?? false;
 	const color = props?.color?.value ?? props?.color ?? '#efefef';
 
+	function hexToRGBA(hex: string, alpha: number): string {
+		hex = hex.replace(/^#/, '');
+
+		if (hex.length === 3) {
+			hex = hex.split('').map(char => char + char).join('');
+		}
+
+		const bigint = parseInt(hex, 16);
+		const r = (bigint >> 16) & 255;
+		const g = (bigint >> 8) & 255;
+		const b = bigint & 255;
+
+		return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+	}
+
 	return (
 		<div className={`cdContainer`}>
 			<span className="colorDisplayBlock" style={{backgroundColor:color}}></span>
@@ -15,7 +30,7 @@ export default function ColorDisplay (props:any) {
 					{props?.color?.theme??''} &rarr; {props?.color?.token??''}
 				</div>
 				:
-				<div>-</div>
+				<div>{hexToRGBA(color,0)}</div>
 				}
 			</div>
 		</div>
