0e20f8f816e0607e3b5aa4facebf462bc370ba88
thinkdj <think.dj@gmail.com>
2023-10-19 10:58:42 +0530

    next.config: export for local/prod

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     basePath: '',
-    output: 'export',
+    output: isProduction?'export':'',
 }
 
 module.exports = nextConfig
